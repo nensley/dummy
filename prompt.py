184d5910183ad52ddc4309fe1bf43bfb86725fa5
nensley <122550104+nensley@users.noreply.github.com>
2024-10-23 02:23:26 -0400

    Updating the Prompt

--- prompt.py
+++ prompt.py
@@ -2,10 +2,13 @@
 from .text_utils import read_file, count_word_occurrences
 
 # makes a function that analyzes token values in file
-def run_prompt(file_path, token):
+def report_count(token):
+    # define file path
+    file_path = 'corpus.txt'
+
     # generates a prompt by reading a file and counting occurrences of token
     text = read_file(file_path)  # Read the corpus file
-    # count 
+    # count occurrences of the token in the text
     count = count_word_occurrences(text, token)
     
     # run the prompt
